3337ffeb5f5d1c14df88543bdee1bf3a45189a06
haile17 <hailefikere17@gmail.com>
2022-10-24 10:46:55 +0300

    AirBnB_clone_v3

--- api/views/index.py
+++ api/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """index.py to connect to API"""
-import app_views from api.v1.views
+from api.v1.views import app_views
 hbnbClass = {
     'Amenity': Amenity,
     'City': City,
